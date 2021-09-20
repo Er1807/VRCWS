c1291f9aab60e1cb2bcbd52b5fc2d3e84c9147d9
Eric van Fandenfart <ruhlanderic@gmail.com>
2021-09-20 04:46:47 +0200

    Split from textChat

---
+++ Server/Program.cs
@@ -0,0 +1,133 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace Server
+{
+    public class VRCWS : WebSocketBehavior
+    {
+        public class Message
+        {
+            public string Method { get; set; }
+            public string Target { get; set; }
+            public string Content { get; set; }
+
+            public override string ToString()
+            {
+                return $"{Method} - {Target} - {Content}";
+            }
+        }
+
+        public static Dictionary<string, VRCWS> userIDToVRCWS = new Dictionary<string, VRCWS>();
+
+        public string userID;
+        public List<string> acceptableMethods = new List<string>();
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Message msg =  JsonConvert.DeserializeObject<Message>(e.Data);
+            Console.WriteLine($"<< {msg}");
+            if(msg.Method == "StartConnection")
+            {
+                if (userIDToVRCWS.ContainsKey(msg.Content)){
+                    Send(new Message() { Method = "Error", Content = "AlreadyConnected" });
+                    return;
+                }
+                userID = msg.Content;
+                userIDToVRCWS[userID] = this;
+                Send(new Message() { Method = "Connected" });
+                return;
+            }
+
+            if (userID == null)
+            {
+                Send(new Message() { Method = "Error", Content = "StartConnectionFirst" });
+                return;
+            }
+
+            if (msg.Method == "AcceptMethod")
+            {
+                acceptableMethods.Add(msg.Content);
+                Send(new Message() { Method = "MethodsUpdated" });
+            }
+            else if(msg.Method == "RemoveMethod")
+            {
+                acceptableMethods.Remove(msg.Content);
+                Send(new Message() { Method = "MethodsUpdated"});
+            }
+            else if (msg.Method == "IsOnline")
+            {
+                if (userIDToVRCWS.ContainsKey(msg.Target)){
+                    Send(new Message() { Method = "OnlineStatus", Target = msg.Target, Content = "Online" });
+                }
+                else
+                {
+                    Send(new Message() { Method = "OnlineStatus", Target = msg.Target, Content = "Offline" });
+                }
+            } 
+            else
+            {
+                if (!userIDToVRCWS.ContainsKey(msg.Target)) {
+                    Send(new Message() { Method = "Error", Target = msg.Target, Content = "UserOffline" });
+                    return;
+                }
+                var remoteUser = userIDToVRCWS[msg.Target];
+                if(!remoteUser.acceptableMethods.Contains(msg.Method))
+                {
+                    Send(new Message() { Method = "Error", Target = msg.Target, Content = "MethodNotAcepted" });
+                    return;
+                }
+                msg.Target = userID;
+                remoteUser.Send(msg);
+            }
+        }
+
+        protected override void OnClose(CloseEventArgs e)
+        {
+            if (userID == null) return;
+            Console.WriteLine($"User {userID} dissconected");
+                userIDToVRCWS.Remove(userID);
+        }
+
+        protected override void OnError(ErrorEventArgs e)
+        {
+            if (userID == null) return;
+            Console.WriteLine($"User {userID} errored");
+            userIDToVRCWS.Remove(userID);
+        }
+
+        public void Send(Message msg)
+        {
+            Console.WriteLine($">> {msg}");
+            Send(JsonConvert.SerializeObject(msg));
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var exitEvent = new ManualResetEvent(false);
+            Console.CancelKeyPress += (sender, eventArgs) => {
+                eventArgs.Cancel = true;
+                exitEvent.Set();
+            };
+
+            var wssv = new WebSocketServer("ws://0.0.0.0:8080");
+            wssv.AddWebSocketService<VRCWS>("/VRC");
+            wssv.AllowForwardedRequest = true;
+            
+            wssv.Start();
+            Console.WriteLine("Listening");
+            exitEvent.WaitOne();
+            wssv.Stop();
+        }
+    }
+}
---
+++ VRCWS/Client.cs
@@ -0,0 +1,195 @@
+﻿using MelonLoader;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using VRCWS;
+
+[assembly: MelonInfo(typeof(VRCWSLibaryMod), "VRCWSLibary", "1.0.0", "Eric van Fandenfart")]
+[assembly: MelonGame]
+
+
+namespace VRCWS
+{
+    
+
+    public class Message
+    {
+        public string Method { get; set; }
+        public string Target { get; set; }
+        public string Content { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Method} - {Target} - {Content}";
+        }
+    }
+
+    public class VRCWSLibaryMod : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            var category = MelonPreferences.CreateCategory("WSConnectionLibary");
+            MelonPreferences_Entry<string> entry =  category.CreateEntry("Server", "wss://vrcws.er1807.de/VRC");
+            entry.OnValueChanged += (oldValue, newValue) => { Client.GetClient().Connect(newValue); };
+            Client.GetClient().Connect(entry.Value);
+        }
+    }
+
+    public class Client
+    {
+        private static Client client;
+
+        public static Client GetClient()
+        {
+            if (client == null) 
+                client = new Client();
+            return client;
+        }
+
+        public static bool ClientAvailable() => client != null && client.connected;
+
+        private ClientWebSocket ws;
+        public event MessageEvent MessageRecieved;
+        public event ConnectEvent Connected;
+        public bool connected = false;
+        public event MessageEvent ErrorRecieved;
+        public event OnlineEvent OnlineRecieved;
+        public delegate void MessageEvent(Message message);
+        public delegate void ConnectEvent();
+        public delegate void OnlineEvent(string userID, bool online);
+
+        public Dictionary<string, MessageEvent> Methods = new Dictionary<string, MessageEvent>();
+
+        
+        public Client()
+        {
+            Connected += () => {
+                foreach (var item in Methods.Keys)
+                {
+                    AcceptMethod(item);
+                }
+            };
+        }
+
+        public void Connect(string server)
+        {
+            MelonLogger.Msg($"Connecting to {server}");
+            connected = false;
+            if(ws!= null && ws.State == WebSocketState.Open)
+            {
+                ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "Restart", CancellationToken.None).Wait();
+            }
+
+            ws = new ClientWebSocket();
+            ws.ConnectAsync(new Uri(server), CancellationToken.None).Wait();
+            Task.Run(() => {
+                Recieve();
+            });
+            MelonCoroutines.Start(SetUserID());
+            MelonLogger.Msg($"Connected to {server}");
+        }
+
+        //https://forum.unity.com/threads/solved-dictionary-of-delegate-such-that-each-value-hold-multiple-methods.506880/
+        public void RegisterEvent(string method, MessageEvent e)
+        {
+            MelonLogger.Msg($"Registering Event {method}");
+            MessageEvent EventStored;
+            if (Methods.TryGetValue(method, out EventStored))
+            {
+                EventStored += e;
+                Methods[method] = EventStored; // Copy the newly aggregated delegate back into the dictionary.
+            }
+            else
+            {
+                EventStored += e;
+                Methods.Add(method, EventStored);
+                if(connected)
+                    AcceptMethod(method);
+            }
+        }
+        private void AcceptMethod(string method)
+        {
+            Send(new Message() { Method = "AcceptMethod", Content = method });
+        }
+        private void RemoveMethod(string method)
+        {
+            Send(new Message() { Method = "RemoveMethod", Content = method });
+        }
+
+        public void IsUserOnline(string userID)
+        {
+            Send(new Message() { Method = "IsOnline", Target = userID});
+        }
+
+        public void Send(Message msg)
+        {
+            Send(JsonConvert.SerializeObject(msg));
+        }
+        private void Send(string msg)
+        {
+            if(ws.State == WebSocketState.Open)
+            {
+                ws.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(msg)), WebSocketMessageType.Text, true, CancellationToken.None);
+            }
+        }
+        public IEnumerator SetUserID()
+        {
+            while (VRCPlayer.field_Internal_Static_VRCPlayer_0 == null || VRCPlayer.field_Internal_Static_VRCPlayer_0.prop_String_2 == null)
+                yield return null;
+
+            string userID = VRCPlayer.field_Internal_Static_VRCPlayer_0.prop_String_2;
+            Send(new Message() { Method = "StartConnection", Content = userID });
+        }
+        private async void Recieve()
+        {
+            try
+            {
+                const int maxMessageSize = 1024;
+                byte[] receiveBuffer = new byte[maxMessageSize];
+                while (ws.State == WebSocketState.Open)
+                {
+                    WebSocketReceiveResult receiveResult = await ws.ReceiveAsync(new ArraySegment<byte>(receiveBuffer), CancellationToken.None);
+                    if (receiveResult.MessageType == WebSocketMessageType.Text)
+                    {
+                        var receivedString = Encoding.UTF8.GetString(receiveBuffer, 0, receiveResult.Count);
+                        MelonLogger.Msg(receivedString);
+                        Message msg = JsonConvert.DeserializeObject<Message>(receivedString);
+                        MessageRecieved?.Invoke(msg);
+                        if (msg.Method == "OnlineStatus")
+                        {
+                            OnlineRecieved?.Invoke(msg.Target, msg.Content == "Online");
+                        }
+                        else if (msg.Method == "Error")
+                        {
+                            ErrorRecieved?.Invoke(msg);
+                        }
+                        else if (msg.Method == "MethodsUpdated")
+                        {
+                            //Nothing
+                        }
+                        else if (msg.Method == "Connected")
+                        {
+                            connected = true;
+                            Connected?.Invoke();
+                        }
+                        else
+                        {
+                            if (Methods.ContainsKey(msg.Method))
+                                Methods[msg.Method]?.Invoke(msg);
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MelonLogger.Msg("Reciever errored");
+            }
+        }
+    }
+}
