b19ea45821f1f3cb54e0dd8581a08b4cd9b458dc
Eric van Fandenfart <ruhlanderic@gmail.com>
2021-10-18 19:00:54 +0200

    tset

--- VRCWS/SecurityContext.cs
+++ VRCWS/SecurityContext.cs
@@ -1,70 +1,54 @@
-﻿using MelonLoader;
+﻿
+using MelonLoader;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Security.Cryptography;
 using System.Text;
-
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Prng;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Math;
 namespace VRCWSLibary
 {
     public class SecurityContext
     {
-        //Thanks Requi 
-        public class RSAParametersWrapper{
+        public class RSAParametersWrapper
+        {
 
-            public static RSAParametersWrapper FromRSAParameters(RSAParameters rsa)
+            public static RSAParametersWrapper FromRSAParameters(RsaKeyParameters rsa)
             {
                 return new RSAParametersWrapper()
                 {
-                    Exponent = rsa.Exponent,
-                    Modulus = rsa.Modulus,
-                    P = rsa.P,
-                    Q = rsa.Q,
-                    DP = rsa.DP,
-                    DQ = rsa.DQ,
-                    InverseQ = rsa.InverseQ,
-                    D = rsa.D
+                    Exponent = rsa.Exponent.ToString(16),
+                    Modulus = rsa.Modulus.ToString(16)
                 };
             }
 
-            public static RSAParameters FromRSAParameters(RSAParametersWrapper rsa)
+            public static RsaKeyParameters FromRSAParameters(RSAParametersWrapper rsa, bool isPrivate)
             {
-                return new RSAParameters()
-                {
-                    Exponent = rsa.Exponent,
-                    Modulus = rsa.Modulus,
-                    P = rsa.P,
-                    Q = rsa.Q,
-                    DP = rsa.DP,
-                    DQ = rsa.DQ,
-                    InverseQ = rsa.InverseQ,
-                    D = rsa.D
-                };
+                return new RsaKeyParameters(isPrivate, new BigInteger(rsa.Modulus, 16), new BigInteger(rsa.Exponent, 16));
             }
 
-            public byte[] Exponent;
-            public byte[] Modulus;
-            public byte[] P;
-            public byte[] Q;
-            public byte[] DP;
-            public byte[] DQ;
-            public byte[] InverseQ;
-            public byte[] D;
+            public string Exponent;
+            public string Modulus;
         }
 
-        private static RSACryptoServiceProvider provider;
+        private static RsaKeyParameters publicKey { get; set; }
+        public static RsaKeyParameters privateKey { get; set; }
 
-        public static RSAParametersWrapper privKey;
-        public static RSAParametersWrapper pubKey;
-
-        public static Dictionary<string, RSAParameters> keysPerUser = new Dictionary<string, RSAParameters>();
+        public static Dictionary<string, RSAParametersWrapper> keysPerUser = new Dictionary<string, RSAParametersWrapper>();
 
         private static void CreateKeys(){
-            provider = new RSACryptoServiceProvider(2048);
+            var kpgen = new RsaKeyPairGenerator();
 
-            privKey = RSAParametersWrapper.FromRSAParameters(provider.ExportParameters(true));
-            pubKey = RSAParametersWrapper.FromRSAParameters(provider.ExportParameters(false));
+            kpgen.Init(new KeyGenerationParameters(new SecureRandom(new CryptoApiRandomGenerator()), 2048));
+            var keyPair = kpgen.GenerateKeyPair();
+
+            privateKey = (RsaKeyParameters)keyPair.Private;
+            publicKey = (RsaKeyParameters)keyPair.Public;
 
             MelonLogger.Msg("Created new keys");
 
@@ -72,33 +56,43 @@ namespace VRCWSLibary
 
         public static void AcceptPubKey(string pubkey, string userId)
         {
-            keysPerUser[userId] = JsonConvert.DeserializeObject<RSAParameters>(pubkey);
+            try
+            {
 
-            MelonLogger.Msg($"Accepted key for user {userId}");
-            SaveKeys();
+                keysPerUser[userId] = JsonConvert.DeserializeObject<RSAParametersWrapper>(pubkey);
+                MelonLogger.Msg($"Accepted key for user {userId}");
+                SavePublicKeys();
+            }
+            catch (Exception)
+            {
+                MelonLogger.Msg("Invalid Json Package for PubKey");
+            }
+
         }
 
 
         public static void SaveKeys()
         {
             Directory.CreateDirectory("UserData/VRCWS");
-            File.WriteAllText("UserData/VRCWS/privkey.json", JsonConvert.SerializeObject(privKey));
-            File.WriteAllText("UserData/VRCWS/pubkeydictionary.json", JsonConvert.SerializeObject(keysPerUser));
+            File.WriteAllText("UserData/VRCWS/privatekey.json", JsonConvert.SerializeObject(RSAParametersWrapper.FromRSAParameters(privateKey)));
+            File.WriteAllText("UserData/VRCWS/publickey.json", JsonConvert.SerializeObject(RSAParametersWrapper.FromRSAParameters(publicKey)));
             MelonLogger.Msg("Saving keys");
         }
+        public static void SavePublicKeys()
+        {
+            Directory.CreateDirectory("UserData/VRCWS");
+            File.WriteAllText("UserData/VRCWS/publickeydictionary.json", JsonConvert.SerializeObject(keysPerUser));
+            MelonLogger.Msg("Saving public keys");
+        }
+
+
 
         public static void LoadKeys()
         {
-            if (File.Exists("UserData/VRCWS/privkey.json"))
+            if (File.Exists("UserData/VRCWS/privatekey.json") && File.Exists("UserData/VRCWS/publickey.json"))
             {
-                privKey = JsonConvert.DeserializeObject<RSAParametersWrapper>(File.ReadAllText("UserData/VRCWS/privkey.json"));
-                keysPerUser = JsonConvert.DeserializeObject<Dictionary<string, RSAParameters>>(File.ReadAllText("UserData/VRCWS/pubkeydictionary.json"));
-                MelonLogger.Msg(keysPerUser.Count);
-                provider = new RSACryptoServiceProvider();
-
-                provider.ImportParameters(RSAParametersWrapper.FromRSAParameters(privKey));
-
-                pubKey = RSAParametersWrapper.FromRSAParameters(provider.ExportParameters(false));
+                privateKey = RSAParametersWrapper.FromRSAParameters(JsonConvert.DeserializeObject<RSAParametersWrapper>(File.ReadAllText("UserData/VRCWS/privatekey.json")), true);
+                publicKey = RSAParametersWrapper.FromRSAParameters(JsonConvert.DeserializeObject<RSAParametersWrapper>(File.ReadAllText("UserData/VRCWS/publickey.json")), false);
 
             }
             else
@@ -106,12 +100,19 @@ namespace VRCWSLibary
                 CreateKeys();
                 SaveKeys();
             }
+
+            if (File.Exists("UserData/VRCWS/publickeydictionary.json"))
+            {
+                keysPerUser = JsonConvert.DeserializeObject<Dictionary<string, RSAParametersWrapper>>(File.ReadAllText("UserData/VRCWS/publickeydictionary.json"));
+                MelonLogger.Msg($"Loaded {keysPerUser.Count} public keys");
+            }
+
             MelonLogger.Msg("Loaded keys");
         }
 
         public static string GetPublicKeyAsJsonString()
         {
-            return JsonConvert.SerializeObject(pubKey);
+            return JsonConvert.SerializeObject(RSAParametersWrapper.FromRSAParameters(publicKey));
         }
 
         public static void Sign(Message msg)
@@ -121,23 +122,43 @@ namespace VRCWSLibary
 
         public static string Sign(string data)
         {
-            return GetBase64FromBytes(provider.SignData(GetStringASBytes(data), SHA256.Create()));
+            ISigner sig = SignerUtilities.GetSigner("SHA256withRSA");
+
+            sig.Init(true, privateKey);
+            MelonLogger.Msg(data);
+            var bytes = GetStringASBytes(data);
+
+            sig.BlockUpdate(bytes, 0, bytes.Length);
+            byte[] signature = sig.GenerateSignature();
+            return GetBase64FromBytes(signature);
         }
         public static bool Verify(Message msg)
         {
-            if (!keysPerUser.ContainsKey(msg.Target))
+            if (!keysPerUser.ContainsKey(msg.Target)) {
+                MelonLogger.Msg("Key not found");
                 return false;
-            if (DateTime.Now > msg.TimeStamp.AddSeconds(30))
+            }
+            if (DateTime.UtcNow > msg.TimeStamp.AddSeconds(30))
+            {
+                MelonLogger.Msg("Message expired");
                 return false;
+            }
             return Verify(GetMessageAsString(msg), GetBytesFromBase64(msg.Signature), keysPerUser[msg.Target]);
 
         }
-        public static bool Verify(string data, byte[] signature, RSAParameters remotePubkey)
+        public static bool Verify(string data, byte[] signature, RSAParametersWrapper remotePubkey)
         {
-            var rsa = new RSACryptoServiceProvider();
-            rsa.ImportParameters(remotePubkey);
-            return rsa.VerifyData(GetStringASBytes(data), SHA256.Create(), signature);
+            ISigner signer = SignerUtilities.GetSigner("SHA256withRSA");
 
+            signer.Init(false, RSAParametersWrapper.FromRSAParameters(remotePubkey, false));
+
+            MelonLogger.Msg(data);
+            var msgBytes = GetStringASBytes(data);
+
+
+            signer.BlockUpdate(msgBytes, 0, msgBytes.Length);
+
+            return signer.VerifySignature(signature);
         }
 
         private static string GetMessageAsString(Message msg)
@@ -163,5 +184,8 @@ namespace VRCWSLibary
         {
             return Convert.ToBase64String(data);
         }
+
+
+
     }
 }
